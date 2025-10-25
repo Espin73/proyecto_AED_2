62aedc2b98f44c4f7068bf31e533b3612111c0f8
Espin <espinmartinezjuanantonio@gmail.com>
2025-10-25 12:31:00 +0200

    falta el main

--- 003.cpp
+++ 003.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Fecha {
 private:
     int dia, mes, anio;
-    int hora, mes, segundo;
+    int hora, minuto, segundo;
 
 public:
     Fecha();
@@ -12,8 +12,8 @@ public:
     void escribir();
     void es_menor(Fecha &otra);
     void es_igual(Fecha &otra);
-
-Fecha:Fecha(){
+};
+Fecha::Fecha(){
     dia=0;
     mes=0;
     anio=0;
@@ -22,17 +22,23 @@ Fecha:Fecha(){
     segundo=0;
 }
 
-void Fecha:leer(){
+bool Fecha::leer(){
 
     char b1,b2;
     char p1,p2;
-
+    char espacio;
+    
     cin >> dia >> b1 >> mes >> b2 >> anio;
-    cin >> hora >> p1 >> segundo; 
+    cin.get(espacio); 
+    cin >> hora >> p1 >> minuto >> p2 >> segundo;
 
+    if( b1 != '/' || b2 != '/' || p1 != ':' || p2 != ':'){
+        return false;
+    }
+    return true;
 }
 
-void Fecha:escribir(){
+void Fecha::escribir(){
 
     cout << dia << "/" << mes << "/" << anio << " ";
 
@@ -47,4 +53,41 @@ void Fecha:escribir(){
     if(segundo<10){
         cout << "0" << segundo;}
     else{cout << segundo;}
+}
+
+bool Fecha::es_igual(Fecha &otro){
+
+    return dia==otro.dia && mes==otro.mes && anio==otro.anio && hora==otro.hora && minuto==otro.minuto && segundo==otro.segundo;
+}
+
+bool Fecha::es_menor(Fecha &otro){
+    
+    if(anio < otro.anio){
+        return true;
+    }
+    else if(anio == otro.anio){
+        if(mes < otro.mes){
+            return true;
+        }
+        else if(mes == otro.mes){
+            if(dia < otro.dia){
+                return true;
+            }
+            else if(dia == otro.dia){
+                
+                if(hora < otro.hora){
+                    return true;
+                }
+                else if(hora == otro.hora){
+                    if(minuto < otro.minuto){
+                        return true;
+                    }
+                    else if(minuto == otro.minuto){
+                        return segundo < otro.segundo;
+                    }
+                }
+            }
+        }
+    }
+    return false;
 }
