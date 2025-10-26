56c35844f16ad71544096833b079bee2d8877b76
Espin <espinmartinezjuanantonio@gmail.com>
2025-10-26 19:26:14 +0100

    terminado

--- 003.cpp
+++ 003.cpp
@@ -10,8 +10,8 @@ public:
     Fecha();
     bool leer();
     void escribir();
-    void es_menor(Fecha &otra);
-    void es_igual(Fecha &otra);
+    bool es_menor(Fecha &otra);
+    bool es_igual(Fecha &otra);
 };
 Fecha::Fecha(){
     dia=0;
@@ -39,55 +39,91 @@ bool Fecha::leer(){
 }
 
 void Fecha::escribir(){
-
     cout << dia << "/" << mes << "/" << anio << " ";
-
-    if(hora < 10){
-        cout << "0" << hora;}
-    else{cout << hora;}
-
-    if(minuto<10){
-        cout << "0" << minuto;}
-    else{cout << minuto;}
-
-    if(segundo<10){
-        cout << "0" << segundo;}
-    else{cout << segundo;}
-}
-
-bool Fecha::es_igual(Fecha &otro){
-
-    return dia==otro.dia && mes==otro.mes && anio==otro.anio && hora==otro.hora && minuto==otro.minuto && segundo==otro.segundo;
-}
-
-bool Fecha::es_menor(Fecha &otro){
     
-    if(anio < otro.anio){
+    if(hora < 10) cout << '0' << hora;
+    else cout << hora;
+    cout << ':';
+
+    if(minuto < 10) cout << '0' << minuto;
+    else cout << minuto;
+    cout << ':';
+
+    if(segundo < 10) cout << '0' << segundo;
+    else cout << segundo;
+}
+
+bool Fecha::es_igual(Fecha &otra){
+
+    return dia==otra.dia && mes==otra.mes && anio==otra.anio && hora==otra.hora && minuto==otra.minuto && segundo==otra.segundo;
+}
+
+bool Fecha::es_menor(Fecha &otra){
+    
+    if(anio < otra.anio){
         return true;
     }
-    else if(anio == otro.anio){
-        if(mes < otro.mes){
+    else if(anio == otra.anio){
+        if(mes < otra.mes){
             return true;
         }
-        else if(mes == otro.mes){
-            if(dia < otro.dia){
+        else if(mes == otra.mes){
+            if(dia < otra.dia){
                 return true;
             }
-            else if(dia == otro.dia){
+            else if(dia == otra.dia){
                 
-                if(hora < otro.hora){
+                if(hora < otra.hora){
                     return true;
                 }
-                else if(hora == otro.hora){
-                    if(minuto < otro.minuto){
+                else if(hora == otra.hora){
+                    if(minuto < otra.minuto){
                         return true;
                     }
-                    else if(minuto == otro.minuto){
-                        return segundo < otro.segundo;
+                    else if(minuto == otra.minuto){
+                        return segundo < otra.segundo;
                     }
                 }
             }
         }
     }
     return false;
+}
+
+int main(){
+
+    int N;
+    cin >> N;
+
+    Fecha primera;
+
+    if(!primera.leer()){
+        return 1;
+    }
+
+    for(int i = 1; i < N; ++i){
+        Fecha segunda;
+        if(!segunda.leer()) return 1;
+
+        if(segunda.es_menor(primera)){
+            segunda.escribir();
+            cout << " ES ANTERIOR A ";
+            primera.escribir();
+        }
+        else if(segunda.es_igual(primera)){
+            segunda.escribir();
+            cout << " ES IGUAL A ";
+            primera.escribir();
+        }
+        else{
+            segunda.escribir();
+            cout << " ES POSTERIOR A ";
+            primera.escribir();
+        }
+        cout << '\n';
+
+        primera = segunda;
+    }
+    
+    return 0;
 }
